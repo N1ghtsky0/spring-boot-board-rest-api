773000db7b24a32724e13363980066a4f02ca3a6
N1ghtsky0 <20korea01@gmail.com>
2025-10-22 13:10:14 +0900

    first commit

---
+++ src/test/java/xyz/jiwook/demo/springBootBoardRestApi/SpringBootBoardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jiwook.demo.springBootBoardRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
